17900b7cce23a8c78c7198480623c772c97edf54
Michael du Breuil <wicked.shell.scripts@gmail.com>
2017-06-26 19:03:30 -0700

    Fix jar loading, update README to point at semaphore

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # clj-ardupilot-utils
 
+[![Build Status](https://semaphoreci.com/api/v1/diydrones/clj-ardupilot-utils/branches/master/badge.svg)](https://semaphoreci.com/diydrones/clj-ardupilot-utils)
+
 A Clojure library of helper utilities for use with [Ardupilot](https://github.com/ArduPilot/ardupilot/).
 
 ## Usage
--- test/ardupilot_utils/log_parser_test.clj
+++ test/ardupilot_utils/log_parser_test.clj
@@ -1,9 +1,13 @@
 (ns ardupilot-utils.log-parser-test
   (:require [ardupilot-utils.log-reader :refer :all]
             [clojure.java.io :as io]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all])
+  (:import [java.util.zip ZipEntry]
+           [java.util.jar JarFile]))
 
 (deftest large-file
   (testing "Testing a large file of valid input"
-    (with-open [bad-input (io/input-stream (io/resource "176.BIN"))]
-      (is (= 3886213 (count (parse-bin bad-input)))))))
+           (let [jar (new JarFile (io/file (io/resource "176.jar")))
+                 entry (.getEntry jar "176.BIN")]
+             (with-open [input (.getInputStream jar entry)]
+               (is (= 3886213 (count (parse-bin input))))))))
