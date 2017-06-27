2015345fb3fb04a853e2cbc4dba7e626f1ab5967
Michael du Breuil <wicked.shell.scripts@gmail.com>
2017-06-27 11:28:46 -0700

    Return a lazy sequence from parse-bin
    Allows filtering and a reduced memory footprint

--- src/ardupilot_utils/log_reader.clj
+++ src/ardupilot_utils/log_reader.clj
@@ -6,32 +6,31 @@
              [java.io EOFException]))
 
 (defn parse-bin
-  "Parse a bin file returning a map of messages to values.
+  "Parse a bin file returning a lazy sequeonce of message maps.
    Any log corruption, or unexpected ending will be ingored, and the parser
    will attempt to recover parsing the stream"
-  [input]
-  (with-open [reader (new LittleEndianDataInputStream input)]
-    (loop [message-id (find-next-message reader)
-           formats {128 {:length 89
-                         :name "FMT"
-                         :fields [{:name :Type    :type \B}
-                                  {:name :Length  :type \B}
-                                  {:name :Name    :type \n}
-                                  {:name :Format  :type \N}
-                                  {:name :Columns :type \Z}]}}
-           messages (transient [])]
-          (if message-id
-            (let [{:keys [fields] :as message-format} (get formats message-id)]
-              (when-not message-format
-                (throw (ex-info "Unknown message format" {:message-id message-id})))
-              (if-let [message (try
-                                   ; attempt to read all the fields out of the message
-                                   (persistent! (reduce (fn [read-fields {:keys [name type]}]
-                                                            (assoc! read-fields name (read-field type reader)))
-                                                        (transient {}) fields))
-                                   (catch EOFException e))]
-                (if (= message-id FORMAT-MESSAGE-ID)
-                  (recur (find-next-message reader) (merge-format-message formats message) (conj! messages message))
-                  (recur (find-next-message reader) formats (conj! messages message)))
-                (persistent! messages)))
-            (persistent! messages)))))
+  ([input]
+   (let [reader (new LittleEndianDataInputStream input)]
+     (parse-bin reader {128 {:length 89
+                             :name "FMT"
+                             :fields [{:name :Type    :type \B}
+                                      {:name :Length  :type \B}
+                                      {:name :Name    :type \n}
+                                      {:name :Format  :type \N}
+                                      {:name :Columns :type \Z}]}})))
+  ([^LittleEndianDataInputStream reader formats]
+   (lazy-seq
+     (if-let [message-id (find-next-message reader)]
+       (let [{:keys [fields] :as message-format} (get formats message-id)]
+         (when-not message-format
+           (throw (ex-info "Unknown message format" {:message-id message-id})))
+         (if-let [message (try
+                            ; attempt to read all the fields out of the message
+                            (persistent! (reduce (fn [read-fields {:keys [name type]}]
+                                                     (assoc! read-fields name (read-field type reader)))
+                                                 (transient {}) fields))
+                            (catch EOFException e))]
+           (cons message (parse-bin reader (if (= message-id FORMAT-MESSAGE-ID)
+                                             (merge-format-message formats message)
+                                             formats)))
+           (.close reader)))))))
--- test/ardupilot_utils/log_parser_test.clj
+++ test/ardupilot_utils/log_parser_test.clj
@@ -4,10 +4,18 @@
             [clojure.test :refer :all])
   (:import [java.util.zip ZipEntry]
            [java.util.jar JarFile]))
-
 (deftest large-file
   (testing "Testing a large file of valid input"
            (let [jar (new JarFile (io/file (io/resource "176.jar")))
                  entry (.getEntry jar "176.BIN")]
              (with-open [input (.getInputStream jar entry)]
-               (is (= 3886213 (count (parse-bin input))))))))
+               (let [parsed (parse-bin input)]
+                 (is (instance? clojure.lang.LazySeq parsed))
+                 (is (= 3886213 (count parsed))))))))
+
+(deftest filter-accel
+  (testing "Ensure that the result is a lazy sequence and that we can filter it"
+           (let [jar (new JarFile (io/file (io/resource "176.jar")))
+                 entry (.getEntry jar "176.BIN")]
+             (with-open [input (.getInputStream jar entry)]
+               (is (= 497313 (count (filter #(contains? % :AccY) (parse-bin input)))))))))
